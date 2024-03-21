cb78e0e49980ca5c7a573424da1d075b93d3e56d
Kumar Chavan (Conscience) <kumarc@conscienceit.com>
2024-03-21 17:40:36 +0530

    updated

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,22 @@
 
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { list } from '@vercel/blob'
 
-export function middleware(request: NextRequest) {
-    console.log(request.nextUrl)
+export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/@')) {
-    console.log(request.nextUrl.pathname)
+    console.log('From middleware',request.nextUrl.pathname)
+    let url = request.nextUrl.pathname.substring(2)
+    console.log('url',url)
+    let bloblist:any = await list();
+    console.log('bloblist',bloblist)
+    let filter = bloblist.filter((blob)=>url == blob.pathname)
+    console.log('filter',filter[0])
+    if(filter[0]){
+        console.log('yupiee')
+        return NextResponse.redirect(filter[0].url);
+    }
+
     return NextResponse.next()
   }
 }
